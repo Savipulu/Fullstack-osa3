119cbca16aee513e2500b52cd1cbb79d3eb0c793
Savipulu <jouni.winter@gmail.com>
2018-01-25 01:45:52 +0200

    Tehtava 3.1

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+const persons = [
+  {
+    name: "Eka",
+    number: "040-1231234",
+    id: 1
+  },
+  {
+    name: "Toka",
+    number: "040-1231234",
+    id: 2
+  },
+  {
+    name: "Kolmas",
+    number: "040-1231234",
+    id: 3
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("Server running port " + PORT);
+});
+
+/*let persons = [
+    {
+      name: "Eka",
+      number: "040-1231234",
+      id: 1
+    },
+    {
+      name: "Toka",
+      number: "040-1231234",
+      id: 2
+    },
+    {
+      name: "Kolmas",
+      number: "040-1231234",
+      id: 3
+    }
+  ];
+  */ //
