88dbbe27e9f0a616736024ca922c89fa0b12c7ea
Savipulu <jouni.winter@gmail.com>
2018-01-26 13:14:32 +0200

    Tehtava 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     name: "Eka",
     number: "040-1231234",
@@ -48,26 +48,13 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log("Server running port " + PORT);
 });
-
-/*let persons = [
-    {
-      name: "Eka",
-      number: "040-1231234",
-      id: 1
-    },
-    {
-      name: "Toka",
-      number: "040-1231234",
-      id: 2
-    },
-    {
-      name: "Kolmas",
-      number: "040-1231234",
-      id: 3
-    }
-  ];
-  */ //
