f3f62c611a37ca0ce5ce4dbef62b427d282e414c
Savipulu <jouni.winter@gmail.com>
2018-01-27 03:07:20 +0200

    Tehtava 3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://Savipulu:TietoKantaSalaSana@ds217138.mlab.com:17138/fullstack-phonebook'
+
+mongoose.connect(url)
+mongoose.Promise = global.Promise;
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+
+const person = new Person({
+  name: "",
+  number: ""
+})
+
+
+const args = process.argv
+
+if (args.length == 4) {
+    console.log("Lisätään henkilö " + args[2] + " numero " + args[3] + " luetteloon")
+    person.name = args[2]
+    person.number = args[3]
+    person.save().then(result => {
+        console.log("saved person to phonebook")
+        mongoose.connection.close()
+    })
+} else if (args.length == 2) {
+    console.log("puhelinluettelo: ")
+    Person.find({}).then(result => {
+    result.forEach(person => {
+        console.log(person)
+    })
+    mongoose.connection.close()
+})
+} else {
+    mongoose.connection.close()
+}
+  
