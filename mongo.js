9e4878776f530956ce12beef7d3a0de647b0865d
Savipulu <jouni.winter@gmail.com>
2018-01-28 15:29:55 +0200

    .

--- mongo.js
+++ mongo.js
@@ -1,41 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const url = 'mongodb://Savipulu:TietoKantaSalaSana@ds217138.mlab.com:17138/fullstack-phonebook'
+const url = process.env.MONGODB_URI;
 
-mongoose.connect(url)
+mongoose.connect(url);
 mongoose.Promise = global.Promise;
 
-const Person = mongoose.model('Person', {
+const Person = mongoose.model("Person", {
   name: String,
   number: String
-})
-
+});
 
 const person = new Person({
   name: "",
   number: ""
-})
+});
 
-
-const args = process.argv
+const args = process.argv;
 
 if (args.length == 4) {
-    console.log("Lisätään henkilö " + args[2] + " numero " + args[3] + " luetteloon")
-    person.name = args[2]
-    person.number = args[3]
-    person.save().then(result => {
-        console.log("saved person to phonebook")
-        mongoose.connection.close()
-    })
+  console.log(
+    "Lisätään henkilö " + args[2] + " numero " + args[3] + " luetteloon"
+  );
+  person.name = args[2];
+  person.number = args[3];
+  person.save().then(result => {
+    console.log("saved person to phonebook");
+    mongoose.connection.close();
+  });
 } else if (args.length == 2) {
-    console.log("puhelinluettelo: ")
-    Person.find({}).then(result => {
+  console.log("puhelinluettelo: ");
+  Person.find({}).then(result => {
     result.forEach(person => {
-        console.log(person)
-    })
-    mongoose.connection.close()
-})
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
 } else {
-    mongoose.connection.close()
+  mongoose.connection.close();
 }
-  
